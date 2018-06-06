cfad17318e450b3eccf3d96e1db2c96fc40384ed
neillsom <neillsom@mail.com>
2018-06-06 15:31:31 -0700

    user signup, login, auth

--- routes/users.js
+++ routes/users.js
@@ -1,128 +1,73 @@
 'use strict';
 const express = require('express');
-
 const User = require('../models/user');
+
+const bodyParser = require('body-parser');
 
 const router = express.Router();
 
-/* ========== POST/CREATE AN ITEM ========== */
+const jsonParser = bodyParser.json();
 
 
-//favorite style with this id
+// Get all
+router.get('/', (req, res, next) => {
 
-router.post('/:userId/styles/:styleId', (req, res, next) => {
+  User.find()
+    .then(results => {
+      res.json(results);
+    })
+    .catch(err => {
+      next(err)
+    });
 
+});
 
-
-})
-
-
-
-
-
-
-
-
-
+//////
 
 
+// Post to register a new user
+router.post('/', jsonParser, (req, res) => {
 
+  let {username, password, fullname = ''} = req.body;
+  // Username and password come in pre-trimmed, otherwise we throw an error
+  // before this
+  fullname = fullname.trim();
 
-
-router.post('/', (req, res, next) => {
-
-    const requiredFields = ['username', 'password'];
-    const missingField = requiredFields.find(field => !(field in req.body));
-
-    if (missingField) {
-        const err = new Error(`Missing '${missingField}' in request body`);
-        err.status = 422;
-        return next(err);
-    }
-
-    const stringFields = ['username', 'password', 'name'];
-    const nonStringField = stringFields.find(
-        field => field in req.body && typeof req.body[field] !== 'string'
-    );
-
-    if (nonStringField) {
-        const err = new Error(`Field: '${nonStringField}' must be type String`);
-        err.status = 422;
-        return next(err);
-    }
-
-    // If the username and password aren't trimmed we give an error.  Users might
-    // expect that these will work without trimming (i.e. they want the password
-    // "foobar ", including the space at the end).  We need to reject such values
-    // explicitly so the users know what's happening, rather than silently
-    // trimming them and expecting the user to understand.
-    // We'll silently trim the other fields, because they aren't credentials used
-    // to log in, so it's less of a problem.
-    const explicityTrimmedFields = ['username', 'password'];
-    const nonTrimmedField = explicityTrimmedFields.find(
-        field => req.body[field].trim() !== req.body[field]
-    );
-
-    if (nonTrimmedField) {
-        const err = new Error(`Field: '${nonTrimmedField}' cannot start or end with whitespace`);
-        err.status = 422;
-        return next(err);
-    }
-
-    // bcrypt truncates after 72 characters, so let's not give the illusion
-    // of security by storing extra **unused** info
-    const sizedFields = {
-        username: { min: 1 },
-        password: { min: 8, max: 72 }
-    };
-
-    const tooSmallField = Object.keys(sizedFields).find(
-        field => 'min' in sizedFields[field] &&
-      req.body[field].trim().length < sizedFields[field].min
-    );
-    if (tooSmallField) {
-        const min = sizedFields[tooSmallField].min;
-        const err = new Error(`Field: '${tooSmallField}' must be at least ${min} characters long`);
-        err.status = 422;
-        return next(err);
-    }
-
-    const tooLargeField = Object.keys(sizedFields).find(
-        field => 'max' in sizedFields[field] &&
-      req.body[field].trim().length > sizedFields[field].max
-    );
-
-    if (tooLargeField) {
-        const max = sizedFields[tooLargeField].max;
-        const err = new Error(`Field: '${tooSmallField}' must be at most ${max} characters long`);
-        err.status = 422;
-        return next(err);
-    }
-
-    // Username and password were validated as pre-trimmed
-    let { username, password, name = '' } = req.body;
-    name = name.trim();
-
-    // Remove explicit hashPassword if using pre-save middleware
-    return User.hashPassword(password)
-        .then(digest => {
-            const newUser = {
-                username,
-                password: digest,
-                name
-            };
-            return User.create(newUser);
-        })
-        .then(result => {
-            return res.status(201).location(`/api/users/${result.id}`).json(result);
-        })
-        .catch(err => {
-            if (err.code === 11000) {
-                err = new Error('The username already exists');
-                err.status = 400;
-            }
-            next(err);
+  return User.find({username})
+    .count()
+    .then(count => {
+      if (count > 0) {
+        // There is an existing user with the same username
+        return Promise.reject({
+          code: 422,
+          reason: 'ValidationError',
+          message: 'Username already taken',
+          location: 'username'
         });
+      }
+      // If there is no existing user, hash the password
+      return User.hashPassword(password);
+    })
+    .then(hash => {
+      return User.create({
+        username,
+        password: hash,
+        fullname
+      });
+    })
+    .then(user => {
+      return res.status(201).json(user.serialize());
+    })
+
+    .catch(err => {
+      console.log(err)
+      // Forward validation errors on to the client, otherwise give a 500
+      // error because something unexpected has happened
+      if (err.reason === 'ValidationError') {
+        return res.status(err.code).json(err);
+      }
+      res.status(500).json({code: 500, message: 'Internal server error'});
+    });
 });
 
 module.exports = router;
